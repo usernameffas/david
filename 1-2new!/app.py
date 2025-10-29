fa977a863ee36396b10d13f717747394eb7bf500
박지하 <pjh58910@c3r1s2.codyssey.kr>
2025-10-29 20:40:30 +0900

    Feat: 메뉴 화면추가
    추가한 기능 설명
    
    Resolves: #1

---
+++ 1-2new!/app.py
@@ -0,0 +1,17 @@
+# app.py 파일 내용
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 기본 라우트 (최초의 마스터 브랜치에 있었다고 가정)
+@app.route('/')
+def index():
+    return "<h1>Welcome to the Coffee Shop!</h1>"
+
+# 문제 5에서 추가할 /menu 라우트
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

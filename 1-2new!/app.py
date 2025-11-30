7ea9b514041e2630e8e557c96c940615867d12c2
usernameffas <151457809+usernameffas@users.noreply.github.com>
2025-11-30 13:26:07 +0900

    Update app.py

--- 1-2new!/app.py
+++ 1-2new!/app.py
@@ -21,6 +21,9 @@ def index():
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
 
 if __name__ == '__main__':
     # 문제 8: app.run() 실행 시 debug=True 옵션 설정 (기존 코드에 이미 있음)
